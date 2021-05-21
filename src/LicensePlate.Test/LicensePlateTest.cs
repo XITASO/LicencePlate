a121d5222ae9368f228147a1551bfaee9d6fc0fc
Christian Hoefle <christian.hoefle@xitaso.com>
2021-05-21 11:02:40 +0200

    Initial commit

---
+++ src/LicensePlate.Test/LicensePlateTest.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace LicensePlate.Test
+{
+    public class LicensePlateTest
+    {
+        [Theory]
+        [InlineData("A-XI100")]
+        public void CreateLicensePlate_NonEmptyText_LicensePlateCreated(string text)
+        {
+            // Act
+            var licensePlate = new LicensePlate(text);
+            
+            // Assert
+            licensePlate.Text.ShouldBe(text);
+        }
+    }
+}
---
+++ src/LicensePlate/LicensePlate.cs
@@ -0,0 +1,12 @@
+﻿namespace LicensePlate
+{
+    public class LicensePlate
+    {
+        public readonly string Text;
+
+        public LicensePlate(string text)
+        {
+            Text = text;
+        }
+    }
+}
